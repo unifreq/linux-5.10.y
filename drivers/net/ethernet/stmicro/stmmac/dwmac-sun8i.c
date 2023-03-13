0d348ff9f4dc6da90085bb049659886b8f42dc4e
赵紫菜 <25471437+13584452567@users.noreply.github.com>
2023-03-13 18:47:55 +0800

    Use-devm_regulator_get-for-PHY-regu

--- drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-sun8i.c
@@ -548,13 +548,10 @@ static int sun8i_dwmac_init(struct platform_device *pdev, void *priv)
 	struct net_device *ndev = platform_get_drvdata(pdev);
 	struct sunxi_priv_data *gmac = priv;
 	int ret;
-
-	if (gmac->regulator) {
-		ret = regulator_enable(gmac->regulator);
-		if (ret) {
-			dev_err(&pdev->dev, "Fail to enable regulator\n");
-			return ret;
-		}
+	ret = regulator_enable(gmac->regulator);
+	if (ret) {
+		dev_err(&pdev->dev, "Fail to enable regulator\n");
+		return ret;	
 	}
 
 	ret = clk_prepare_enable(gmac->tx_clk);
@@ -1027,7 +1024,6 @@ static void sun8i_dwmac_exit(struct platform_device *pdev, void *priv)
 
 	clk_disable_unprepare(gmac->tx_clk);
 
-	if (gmac->regulator)
 		regulator_disable(gmac->regulator);
 }
 
@@ -1156,12 +1152,13 @@ static int sun8i_dwmac_probe(struct platform_device *pdev)
 	}
 
 	/* Optional regulator for PHY */
-	gmac->regulator = devm_regulator_get_optional(dev, "phy");
+	gmac->regulator = devm_regulator_get(dev, "phy");
 	if (IS_ERR(gmac->regulator)) {
-		if (PTR_ERR(gmac->regulator) == -EPROBE_DEFER)
-			return -EPROBE_DEFER;
-		dev_info(dev, "No regulator found\n");
-		gmac->regulator = NULL;
+		ret = PTR_ERR(gmac->regulator);
+		if (ret != -EPROBE_DEFER)
+			dev_err(dev, "Failed to get PHY regulator (%d)\n", ret);
+		return ret;
+
 	}
 
 	/* The "GMAC clock control" register might be located in the
